e7f7dea945dd8f979db5f8e435f94d16cebf39ff
Marlon Monsores <64220885+monsor3s@users.noreply.github.com>
2022-04-01 15:28:28 -0300

    updt cmmt

--- src/Header/HeaderElements.js
+++ src/Header/HeaderElements.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import { colors } from "../styles/"
 
 export const Head = styled.div`
 
@@ -8,20 +7,21 @@ export const Head = styled.div`
 export const HeaderContainer = styled.div`
     z-index: 99;
     height: 600px;
-    background-color: ${colors.lightRed};
+    background-color: var(--light-red);
     border-radius: 0 0 0 100px;
 `;
 
 export const HeaderTitle = styled.h1`
     font-size: 25px;
-    color: ${colors.white};
+    color: var(--white);
 `;
 
-export const HeaderMenu = styled.ul`
-
+export const HeaderMenu = styled.div`
+    position: absolute;
 `;
 
-export const HeaderItem = styled.li`
+export const HeaderItem = styled.a`
+    
     font-size: 20px;
     color: var(--white);
 `;
